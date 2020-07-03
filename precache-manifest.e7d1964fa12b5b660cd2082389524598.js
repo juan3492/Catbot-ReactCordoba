1c385c065089fa73be85342c6cb8f6658e1cbc67
juan3492 <juanpunky3492@gmail.com>
2020-07-02 22:33:30 -0300

    Updates

---
+++ precache-manifest.e7d1964fa12b5b660cd2082389524598.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba99656a104ea5bcdfa323c34402c7c1",
+    "url": "/Catbot-ReactCordoba/index.html"
+  },
+  {
+    "revision": "6142db61d7e05a85d560",
+    "url": "/Catbot-ReactCordoba/static/css/main.2009a38e.chunk.css"
+  },
+  {
+    "revision": "6d28a3922570b798c395",
+    "url": "/Catbot-ReactCordoba/static/js/2.fbcaeb9a.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/Catbot-ReactCordoba/static/js/2.fbcaeb9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6142db61d7e05a85d560",
+    "url": "/Catbot-ReactCordoba/static/js/main.96af47e3.chunk.js"
+  },
+  {
+    "revision": "30ceff31c619fb1b468b",
+    "url": "/Catbot-ReactCordoba/static/js/runtime-main.be03e275.js"
+  },
+  {
+    "revision": "f9baef8ac0d836e6486419e282e42336",
+    "url": "/Catbot-ReactCordoba/static/media/Quicksand-VariableFont_wght.f9baef8a.ttf"
+  },
+  {
+    "revision": "3ea058184ad079641685b03f55e5010d",
+    "url": "/Catbot-ReactCordoba/static/media/userAvatar.3ea05818.png"
+  }
+]);
